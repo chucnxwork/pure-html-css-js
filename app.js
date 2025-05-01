debb2d6e0f063c0227be31725209f8fd0ebd433e
se-learning <chucnxwork@gmail.com>
2025-05-01 21:24:24 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Check if service workers are supported
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(error => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
